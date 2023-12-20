d8f879ca3bf8e0b0ba4566d26f0983b931a7fcde
Chris Pressland <mail@cpressland.io>
2023-12-20 13:12:33 +0000

    Added metrics for events

--- snowstorm/deployments/event_processor.py
+++ snowstorm/deployments/event_processor.py
@@ -2,16 +2,23 @@ from kombu import Connection, Exchange, Queue
 from kombu.mixins import ConsumerMixin
 from kombu.transport.pyamqp import Message
 from loguru import logger
+from prometheus_client import Counter, start_wsgi_server
 from sqlalchemy.orm import Session
 
 from snowstorm.database import Events, engine
 from snowstorm.settings import settings
+
+c = Counter("snowstorm_events", "Number of events processed", ["event_type"])
 
 
 class Deploy_EventProcessor(ConsumerMixin):
     def __init__(self, queues: str):
         self.queues = [Queue(queue, Exchange(queue, type="direct"), routing_key=queue) for queue in queues.split(",")]
         self.connection = Connection(str(settings.rabbitmq_dsn))
+        try:
+            start_wsgi_server(9100)
+        except OSError:
+            logger.warning("Prometheus metrics server already running")
 
     def get_consumers(self, Consumer, channel):
         return [Consumer(queues=self.queues, auto_declare=False, callbacks=[self.on_message])]
@@ -36,6 +43,7 @@ class Deploy_EventProcessor(ConsumerMixin):
             session.commit()
         if settings.debug_mode:
             logger.info("Event payload: {}", event)
+        c.labels(event_type=event_type).inc()
         message.ack()
 
     def dead_letter(self, body: str) -> None:
